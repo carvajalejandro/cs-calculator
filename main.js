b4de88823d8ace4ae81bf2da79fd3a0eb070a624
carvajalejandro <alejandro.carvajal@mycase.com>
2022-08-24 16:56:37 -0700

    Print out dates user enters in the console

--- main.js
+++ main.js
@@ -8,17 +8,17 @@
 const getValueInput = () =>{
   let inputValue1 = document.getElementById("addDate").value; 
   let inputValue2 = document.getElementById("renewalDate").value; 
-  document.getElementById("valueInput1").textContent = inputValue1; 
-  document.getElementById("valueInput2").textContent = inputValue2; 
+  console.log(inputValue1);
+  console.log(inputValue2);
   return (inputValue1, inputValue2);
 }
 
-let date_1 = new Date('10/25/2021');
-let date_2 = new Date();
+// let date_1 = new Date('10/25/2021');
+// let date_2 = new Date();
 
-const days = (date_1, date_2) =>{
-    let difference = date_1.getTime() - date_2.getTime();
-    let TotalDays = Math.ceil(difference / (1000 * 3600 * 24));
-    return TotalDays;
-}
-console.log(days(date_1, date_2) +" days to world cup");
+// const days = (date_1, date_2) =>{
+//     let difference = date_1.getTime() - date_2.getTime();
+//     let TotalDays = Math.ceil(difference / (1000 * 3600 * 24));
+//     return TotalDays;
+// }
+// console.log(days(date_1, date_2) +" days to world cup");
