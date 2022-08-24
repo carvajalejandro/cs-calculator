de41f7ae8f379413b2ef583a808edc8a239ac7d9
carvajalejandro <alejandro.carvajal@mycase.com>
2022-08-24 16:06:07 -0700

    re-copy and paste code to make it work I dont know why it wasnt working.

---
+++ main.js
@@ -0,0 +1,16 @@
+//Create variables that take the date data from the user input
+//then asign those dates to the code we got from the internet
+//Put the entire code into a function 
+//Create a button that causes the functions to run
+//Display the result in the page
+
+
+const getValueInput = () =>{
+    let inputValue1 = document.getElementById("addDate").value; 
+    let inputValue2 = document.getElementById("renewalDate").value; 
+  document.getElementById("valueInput1").innerHTML = inputValue1; 
+  document.getElementById("valueInput2").innerHTML = inputValue2; 
+}
+
+    
+
