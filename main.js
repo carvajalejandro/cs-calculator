eac512f975f513295f1b5245675caf3bccff08a8
carvajalejandro <alejandro.carvajal@mycase.com>
2022-08-24 19:46:06 -0700

    calculation() works in console with user input.
    Now I have to make it so the result of the function displays on the screen.

--- main.js
+++ main.js
@@ -11,12 +11,15 @@ claculation();
 }
 
 function claculation(){
-  let inputValue1 = document.getElementById("addDate").value; 
-  let inputValue2 = document.getElementById("renewalDate").value; 
-  let date_1 = new Date(`${inputValue1}`);
-  let date_2 = new Date(`${inputValue2}`);
+  let addDate = document.getElementById("addDate").value; 
+  let renewalDate = document.getElementById("renewalDate").value; 
+  let annualPrice = document.getElementById("annualPrice").value; 
+  let date_1 = new Date(`${addDate}`);
+  let date_2 = new Date(`${renewalDate}`);
   let difference = date_2.getTime() - date_1.getTime();
   let totalDays = Math.ceil(difference / (1000 * 3600 * 24)); 
-  console.log(totalDays);
-  return (totalDays);
+  let dayilyCost= annualPrice/356;
+  let renewalCost= totalDays * dayilyCost;
+  console.log(renewalCost);
+  return (renewalCost);
 }
