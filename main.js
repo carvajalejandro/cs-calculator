87b6a8f6e05fbceabc3a503394d5fc90ff2bc51d
carvajalejandro <alejandro.carvajal@mycase.com>
2022-08-24 19:32:54 -0700

    put all the code and variables into a calculation();

--- main.js
+++ main.js
@@ -6,11 +6,17 @@
 
 
 const getValueInput = () =>{
+claculation();
+
+}
+
+function claculation(){
   let inputValue1 = document.getElementById("addDate").value; 
   let inputValue2 = document.getElementById("renewalDate").value; 
   let date_1 = new Date(`${inputValue1}`);
   let date_2 = new Date(`${inputValue2}`);
   let difference = date_2.getTime() - date_1.getTime();
-  let TotalDays = Math.ceil(difference / (1000 * 3600 * 24));
-  return TotalDays;
+  let totalDays = Math.ceil(difference / (1000 * 3600 * 24)); 
+  console.log(totalDays);
+  return (totalDays);
 }
