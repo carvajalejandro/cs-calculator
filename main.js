3d60acbe8ce6876e1e7c35f7b71b4ef0787234d3
carvajalejandro <alejandro.carvajal@mycase.com>
2022-08-25 08:12:15 -0700

    Fix typos

--- main.js
+++ main.js
@@ -17,6 +17,6 @@ function claculation(){
   let final= addCost.toFixed(2)
   document.getElementById("onScreenCost").textContent = `$${final} `;
   document.getElementById("onScreenCost").style.fontSize = "50px";
-  document.getElementById("taxReminder").textContent = 'Remember this does not Potential Tax!';
+  document.getElementById("taxReminder").textContent = 'Remember this does not include Potential Tax!';
   document.getElementById("taxReminder").style.fontSize = "50px";
 }
