fdf6559c5510e81e7835fb58574f12567f3488e2
carvajalejandro <alejandro.carvajal@mycase.com>
2022-08-24 19:27:06 -0700

    Make the function work in the console with user input

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
 //Create variables that take the date data from the user input
-let inputValue1; 
-let inputValue2; 
 //then asign those dates to the code we got from the internet
 //Put the entire code into a function 
 //Create a button that causes the functions to run
@@ -10,19 +8,9 @@ let inputValue2;
 const getValueInput = () =>{
   let inputValue1 = document.getElementById("addDate").value; 
   let inputValue2 = document.getElementById("renewalDate").value; 
-  console.log(inputValue1);
-  console.log(inputValue2);
-  
-  return (inputValue1, inputValue2);
+  let date_1 = new Date(`${inputValue1}`);
+  let date_2 = new Date(`${inputValue2}`);
+  let difference = date_2.getTime() - date_1.getTime();
+  let TotalDays = Math.ceil(difference / (1000 * 3600 * 24));
+  return TotalDays;
 }
-  
-
-let date_1 = new Date(`${inputValue1}`);
-let date_2 = new Date(`${inputValue2}`);
-
-// const days = (date_1, date_2) =>{
-//     let difference = date_1.getTime() - date_2.getTime();
-//     let TotalDays = Math.ceil(difference / (1000 * 3600 * 24));
-//     return TotalDays;
-// }
-// console.log(days(date_1, date_2) +" days to world cup");
